14d7a19e9c0481885a8efabb7e3249e3b9d5fa93
immadisairaj <immadirajendra.sai@gmail.com>
2023-04-03 21:04:03 +0530

    change the default padding setting, update docs

--- lib/src/slide/widgets/caption_slide.dart
+++ lib/src/slide/widgets/caption_slide.dart
@@ -2,33 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:prides/prides.dart';
 
 /// A caption slide is where you write a caption that is shown
-/// at the left bottom corner (with some padding).
+/// at the left bottom corner (with some padding using the
+/// [LayoutBuilder] - bottom: height\*0.12, left: width\*0.05).
 class CaptionSlide extends SlideWidget {
   /// Creates a Caption slide using the [caption] widget with
   /// an optional [background] widget.
   ///
-  /// There is also an option for adjusting the required padding.
+  /// There is also an option for adjusting the required padding
+  /// (left and bottom preferred).
   const CaptionSlide({
     required Widget this.caption,
-    this.padding = const EdgeInsets.only(left: 40, bottom: 80),
+    EdgeInsetsGeometry? padding,
     Widget? background,
     super.key,
   })  : _background = background,
-        _captionText = null;
+        _captionText = null,
+        _padding = padding;
 
   /// Creates a Caption slide with just using the string for caption.
-  /// It uses [Text] with the [TextTheme.displaySmall] style.
+  /// It uses [Text] with the [TextTheme.headlineLarge] style.
   ///
   /// You can always use the defaut constructor of [CaptionSlide] for more
   /// customization.
   const CaptionSlide.fromText({
     required String caption,
-    this.padding = const EdgeInsets.only(left: 40, bottom: 80),
+    EdgeInsetsGeometry? padding,
     Widget? background,
     super.key,
   })  : _background = background,
         caption = null,
-        _captionText = caption;
+        _captionText = caption,
+        _padding = padding;
 
   /// caption widget shown at the left bottom corner
   final Widget? caption;
@@ -36,22 +40,40 @@ class CaptionSlide extends SlideWidget {
   final String? _captionText;
 
   /// padding given to the [caption] widget
-  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry? _padding;
 
   final Widget? _background;
 
   @override
   Widget slide(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomLeft,
-      child: Padding(
-        padding: padding,
-        child: caption ??
-            Text(
-              _captionText!,
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        late EdgeInsetsGeometry padding;
+        // only when padding is null
+        if (_padding == null) {
+          // left and bottom paddings when unbounded size
+          var left = 40.0;
+          var bottom = 80.0;
+          if (constraints.hasBoundedHeight) {
+            bottom = constraints.maxHeight * 0.12;
+          }
+          if (constraints.hasBoundedWidth) {
+            left = constraints.maxWidth * 0.05;
+          }
+          padding = EdgeInsets.only(left: left, bottom: bottom);
+        }
+        return Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: _padding ?? padding,
+            child: caption ??
+                Text(
+                  _captionText!,
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+          ),
+        );
+      },
     );
   }
 
---
+++ test/slides/widgets/caption_slide_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:prides/prides.dart';
+
+import 'widgets_test_helper.dart';
+
+void main() {
+  testWidgets('caption slide test', (tester) async {
+    // TODO: test the caption slide
+  });
+}
